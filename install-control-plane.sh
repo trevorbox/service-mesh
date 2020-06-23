9906b1436fa0435879624d7b4a607b5f0660000a
tbox <tbox@redhat.com>
2020-06-23 13:24:42 -0700

    Added service mesh operator install script

--- install-control-plane.sh
+++ install-control-plane.sh
@@ -15,7 +15,7 @@ RETRY=10
 for ((i=$RETRY; i>=1; i--)); do if [ -z "$(oc get servicemeshcontrolplane ${control_plane_name} -n ${deploy_namespace} -o jsonpath={'.status.annotations.readyComponentCount'})" ]; then echo "wait $i more times for service mesh deployment..."; (( FAILURES++ )); sleep 30s; else echo "service mesh deployed successfully!"; break; fi; done
 
 #exit with failure if the mesh failed to deploy
-if [ $FAILURES -ge $RETRY ]; then exit 1; fi
+if [ $FAILURES -ge $RETRY ]; then echo "Failed to deploy in 5 minutes."; exit 1; fi
 
 echo "Done."
 exit 0
---
+++ install-service-mesh-operators.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+helm install service-mesh-operators -n openshift-operators service-mesh-operators/
+
+echo "Manually approve the InstallPlans within the openshift-operators namespace at the following URL..."
+echo "https://$(oc get route console -o jsonpath={.spec.host} -n openshift-console)/k8s/ns/openshift-operators/operators.coreos.com~v1alpha1~InstallPlan"
